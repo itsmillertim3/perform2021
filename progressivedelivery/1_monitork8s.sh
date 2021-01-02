16871eb3ff8280c08f3d4a1ac676e8ac0552255f
Andreas Grabner <grabnerandi@gmx.at>
2021-01-02 21:01:11 +0100

    fixes
    Signed-off-by: Andreas Grabner <grabnerandi@gmx.at>

--- progressivedelivery/1_monitork8s.sh
+++ progressivedelivery/1_monitork8s.sh
@@ -11,7 +11,7 @@ echo "1. Deploy OneAgent Operator"
 helm repo add dynatrace https://raw.githubusercontent.com/Dynatrace/helm-charts/master/repos/stable
 kubectl create namespace dynatrace
 
-cat > values.yaml <<-EOM platform: "kubernetes"
+cat > values.yaml <<- EOM platform: "kubernetes"
 
 oneagent:
   name: "oneagent"
@@ -35,7 +35,7 @@ echo "2. Setup k8s API Integration"
 kubectl apply -f https://www.dynatrace.com/support/help/codefiles/kubernetes/kubernetes-monitoring-service-account.yaml
 K8SAPI_URL=$(kubectl config view --minify -o jsonpath='{.clusters[0].cluster.server}')
 K8SBEARER_TOKEN=$(kubectl get secret $(kubectl get sa dynatrace-monitoring -o jsonpath='{.secrets[0].name}' -n dynatrace) -o jsonpath='{.data.token}' -n dynatrace | base64 --decode)
-curl -X POST "https://$DT_TENANT" \
+curl -X POST "https://$DT_TENANT/api/config/v1/kubernetes/credentials" \
      -H "accept: application/json; charset=utf-8" \
     -H "Authorization: Api-Token $DT_API_TOKEN" \
     -H "Content-Type: application/json; charset=utf-8" \
