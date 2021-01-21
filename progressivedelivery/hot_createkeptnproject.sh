a159f66162ef0aee6f6604921eca6d55ee833082
Andreas Grabner <grabnerandi@gmx.at>
2021-01-21 17:11:00 +0100

    dashboard for monaco updates
    Signed-off-by: Andreas Grabner <grabnerandi@gmx.at>

--- progressivedelivery/hot_createkeptnproject.sh
+++ progressivedelivery/hot_createkeptnproject.sh
@@ -1,10 +1,29 @@
 #!/bin/bash
 
-if [[ -z "$1" ]]; then
+STUDENTID=${STUDENTID:-$1}
+
+if [[ -z "$STUDENTID" ]]; then
   echo "You have to specify a project name, e.g: studentxxx"
   echo "Usage: $0 student001"
   exit 1
 fi
+
+# The following section will only be executed once to replace some placeholders in our monaco files
+# For the first run we need to make sure we set the owner into the dashboard template
+DT_USERNAME=${DT_USERNAME:-none}
+if [ ! -f simplenode/monaco/projects/simplenode/dashboard/qualitygatedb.yaml ]; then
+  if [[ -z "$DT_USERNAME" ]]; then
+    echo "Please export DT_USERNAME=dynatraceusername as its needed for dashboard owner"
+    exit 1
+  fi
+  sed "s/REPLACE_DASHBOARD_OWNER/$DT_USERNAME/g" simplenode/monaco/projects/simplenode/dashboard/qualitygatedb.yaml.tmpl >> simplenode/monaco/projects/simplenode/dashboard/qualitygatedb.yaml
+fi 
+# If we havent yet prepared the synthetic.yaml we do it now by replacing REPLACE_KEPTN_INGRESS with the actual KEPTN_INGRESS.
+if [ ! -f simplenode/monaco/projects/simplenode/synthetic-monitor/synthetic.yaml ]; then
+  KEPTN_INGRESS=$(kubectl get cm -n keptn ingress-config -ojsonpath={.data.ingress_hostname_suffix})
+  sed "s/REPLACE_KEPTN_INGRESS/$KEPTN_INGRESS/g" simplenode/monaco/projects/simplenode/synthetic-monitor/synthetic.yaml.tmpl >> simplenode/monaco/projects/simplenode/synthetic-monitor/synthetic.yaml
+fi 
+
 
 KEPTN_BRIDGE_URL=$(kubectl get secret dynatrace -n keptn -ojsonpath={.data.KEPTN_BRIDGE_URL} | base64 --decode)
 DT_TENANT=$(kubectl get secret dynatrace -n keptn -ojsonpath={.data.DT_TENANT} | base64 --decode)
@@ -13,7 +32,7 @@ DT_API_TOKEN=$(kubectl get secret dynatrace -n keptn -ojsonpath={.data.DT_API_TO
 SERVICENAME=simplenode
 STAGE_PROD=prod
 STAGE_STAGING=staging
-PROJECTNAME=$1
+PROJECTNAME=$STUDENTID
 
 echo "Create Keptn Project: ${PROJECTNAME} based on shipyard.yaml"
 keptn create project "${PROJECTNAME}" --shipyard=${SERVICENAME}/shipyard.yaml
@@ -35,11 +54,6 @@ keptn add-resource --project="${PROJECTNAME}" --service=${SERVICENAME} --stage=$
 keptn add-resource --project="${PROJECTNAME}" --service=${SERVICENAME} --stage=${STAGE_PROD} --resource=${SERVICENAME}/slo.yaml --resourceUri=slo.yaml
 
 echo "Add monaco configuration for setting up Synthetic Tests for PROD"
-# If we havent yet prepared the synthetic.yaml we do it now by replacing REPLACE_KEPTN_INGRESS with the actual KEPTN_INGRESS.
-if [ ! -f simplenode/monaco/projects/simplenode/synthetic-monitor/synthetic.yaml ]; then
-  KEPTN_INGRESS=$(kubectl get cm -n keptn ingress-config -ojsonpath={.data.ingress_hostname_suffix})
-  sed "s/REPLACE_KEPTN_INGRESS/$KEPTN_INGRESS/g" simplenode/monaco/projects/simplenode/synthetic-monitor/synthetic.yaml.tmpl >> simplenode/monaco/projects/simplenode/synthetic-monitor/synthetic.yaml
-fi 
 keptn add-resource --project="${PROJECTNAME}" --service="${SERVICENAME}" --stage="${STAGE_PROD}" --resource=simplenode/monaco/projects/simplenode/synthetic-monitor/synthetic.yaml --resourceUri=dynatrace/projects/${SERVICENAME}/synthetic-monitor/synthetic.yaml
 keptn add-resource --project="${PROJECTNAME}" --service="${SERVICENAME}" --stage="${STAGE_PROD}" --resource=simplenode/monaco/projects/simplenode/synthetic-monitor/synthetic.json --resourceUri=dynatrace/projects/${SERVICENAME}/synthetic-monitor/synthetic.json
 
@@ -53,13 +67,6 @@ echo "Enable Namespaces Labels & Annotations to be accessible by Dynatrace OneAg
 # https://www.dynatrace.com/support/help/technology-support/cloud-platforms/kubernetes/other-deployments-and-configurations/leverage-tags-defined-in-kubernetes-deployments/
 kubectl -n ${PROJECTNAME}-${STAGE_STAGING} create rolebinding default-view --clusterrole=view --serviceaccount=${PROJECTNAME}-${STAGE_STAGING}:default
 kubectl -n ${PROJECTNAME}-${STAGE_PROD} create rolebinding default-view --clusterrole=view --serviceaccount=${PROJECTNAME}-${STAGE_PROD}:default
-
-echo "Create Dynatrace SLOs for service in production"
-curl -X POST "https://{${DT_TENANT}}/api/v2/slo" \
-     -H "accept: */*" \
-     -H "Authorization: Api-Token ${DT_API_TOKEN}" \
-     -H "Content-Type: application/json; charset=utf-8" \
-     -d "{\"enabled\":true,\"name\":\"Success Rate Simplenode ${PROJECTNAME}\",\"description\":\"${PROJECTNAME} Service Successes Server Rate > 99.98%\",\"evaluatedPercentage\":100,\"errorBudget\":0.02,\"status\":\"SUCCESS\",\"error\":\"NONE\",\"useRateMetric\":true,\"metricRate\":\"builtin:service.successes.server.rate\",\"metricNumerator\":\"\",\"numeratorValue\":0,\"metricDenominator\":\"\",\"denominatorValue\":0,\"target\":99.98,\"warning\":99.99,\"evaluationType\":\"AGGREGATE\",\"timeframe\":\"-1w\",\"filter\":\"type(\\\"SERVICE\\\"),tag(\\\"keptn_project:${PROJECTNAME}\\\"),tag(\\\"keptn_stage:prod\\\"),tag(\\\"keptn_service:simplenode\\\")\"}"
 
 echo "Creating a Git Repository for this project"
 cd setup/gitea
