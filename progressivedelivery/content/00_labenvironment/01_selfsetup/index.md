1cbc127e8d9e86dfc162854ff486b4f21e5832be
Andreas Grabner <grabnerandi@gmx.at>
2021-01-26 08:43:38 +0100

    updates
    Signed-off-by: Andreas Grabner <grabnerandi@gmx.at>

---
+++ progressivedelivery/content/00_labenvironment/01_selfsetup/index.md
@@ -0,0 +1,19 @@
+## Run the lab yourself
+
+While we are providing the lab environment for you its possible for you to run it at a later stage yourself!
+
+For detailed information on how to run the lab yourself please look at the [GitHub repo's readme](https://github.com/Dynatrace/perform-2021-hotday/tree/main/progressivedelivery)
+
+Here the quick overview:
+
+**You need**
+1. A k8s cluster
+2. A Dynatrace Tenant
+3. A host that is connected to the internet and connected to the k8s cluster
+
+**To setup**
+To install the lab environment on your k8s:
+1. Clone the github repository on your host
+2. Execute all scripts under the setup folder!
+
+After that you are good to go to continue with the lab
--- progressivedelivery/hot_initialize.sh
+++ progressivedelivery/hot_initialize.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+KEPTN_INGRESS=$(kubectl get cm -n keptn ingress-config -ojsonpath={.data.ingress_hostname_suffix})
+
 # The following section will only be executed once to replace some placeholders in our monaco files
 # For the first run we need to make sure we set the owner into the dashboard template
 DT_USERNAME=${DT_USERNAME:-none}
@@ -8,11 +10,10 @@ if [ ! -f simplenode/monaco/projects/simplenode/dashboard/qualitygatedb.yaml ]; 
     echo "Please export DT_USERNAME=dynatraceusername as its needed for dashboard owner"
     exit 1
   fi
-  sed "s/REPLACE_DASHBOARD_OWNER/$DT_USERNAME/g" simplenode/monaco/projects/simplenode/dashboard/qualitygatedb.yaml.tmpl >> simplenode/monaco/projects/simplenode/dashboard/qualitygatedb.yaml
+  sed "s/REPLACE_DASHBOARD_OWNER/$DT_USERNAME/g;s/REPLACE_KEPTN_BRIDGE/$KEPTN_INGRESS/g" simplenode/monaco/projects/simplenode/dashboard/qualitygatedb.yaml.tmpl >> simplenode/monaco/projects/simplenode/dashboard/qualitygatedb.yaml
 fi 
 # If we havent yet prepared the synthetic.yaml we do it now by replacing REPLACE_KEPTN_INGRESS with the actual KEPTN_INGRESS.
 if [ ! -f simplenode/monaco/projects/simplenode/synthetic-monitor/synthetic.yaml ]; then
-  KEPTN_INGRESS=$(kubectl get cm -n keptn ingress-config -ojsonpath={.data.ingress_hostname_suffix})
   sed "s/REPLACE_KEPTN_INGRESS/$KEPTN_INGRESS/g" simplenode/monaco/projects/simplenode/synthetic-monitor/synthetic.yaml.tmpl >> simplenode/monaco/projects/simplenode/synthetic-monitor/synthetic.yaml
 fi 
 
